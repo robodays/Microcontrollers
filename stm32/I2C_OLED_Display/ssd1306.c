3e8e87de5fe4bc20966b70ac6bacf78efe272276
robodays <70164230+robodays@users.noreply.github.com>
2022-11-01 05:47:32 +0300

    add new projects

--- stm32/I2C_OLED_Display/ssd1306.c
+++ stm32/I2C_OLED_Display/ssd1306.c
@@ -165,21 +165,21 @@ uint8_t SSD1306_Init(void) {
 
 	/* Init I2C */
 	ssd1306_I2C_Init();
-	
+
 	/* Check if LCD connected to I2C */
 	if (HAL_I2C_IsDeviceReady(&hi2c1, SSD1306_I2C_ADDR, 1, 20000) != HAL_OK) {
 		/* Return false */
 		return 0;
 	}
-	
+
 	/* A little delay */
 	uint32_t p = 2500;
 	while(p>0)
 		p--;
-	
+
 	/* Init LCD */
 	SSD1306_WRITECOMMAND(0xAE); //display off
-	SSD1306_WRITECOMMAND(0x20); //Set Memory Addressing Mode   
+	SSD1306_WRITECOMMAND(0x20); //Set Memory Addressing Mode
 	SSD1306_WRITECOMMAND(0x10); //00,Horizontal Addressing Mode;01,Vertical Addressing Mode;10,Page Addressing Mode (RESET);11,Invalid
 	SSD1306_WRITECOMMAND(0xB0); //Set Page Start Address for Page Addressing Mode,0-7
 	SSD1306_WRITECOMMAND(0xC8); //Set COM Output Scan Direction
@@ -206,35 +206,35 @@ uint8_t SSD1306_Init(void) {
 	SSD1306_WRITECOMMAND(0x8D); //--set DC-DC enable
 	SSD1306_WRITECOMMAND(0x14); //
 	SSD1306_WRITECOMMAND(0xAF); //--turn on SSD1306 panel
-	
+
 
 	SSD1306_WRITECOMMAND(SSD1306_DEACTIVATE_SCROLL);
 
 	/* Clear screen */
 	SSD1306_Fill(SSD1306_COLOR_BLACK);
-	
+
 	/* Update screen */
 	SSD1306_UpdateScreen();
-	
+
 	/* Set default values */
 	SSD1306.CurrentX = 0;
 	SSD1306.CurrentY = 0;
-	
+
 	/* Initialized OK */
 	SSD1306.Initialized = 1;
-	
+
 	/* Return OK */
 	return 1;
 }
 
 void SSD1306_UpdateScreen(void) {
 	uint8_t m;
-	
+
 	for (m = 0; m < 8; m++) {
 		SSD1306_WRITECOMMAND(0xB0 + m);
 		SSD1306_WRITECOMMAND(0x00);
 		SSD1306_WRITECOMMAND(0x10);
-		
+
 		/* Write multi data */
 		ssd1306_I2C_WriteMulti(SSD1306_I2C_ADDR, 0x40, &SSD1306_Buffer[SSD1306_WIDTH * m], SSD1306_WIDTH);
 	}
@@ -242,10 +242,10 @@ void SSD1306_UpdateScreen(void) {
 
 void SSD1306_ToggleInvert(void) {
 	uint16_t i;
-	
+
 	/* Toggle invert */
 	SSD1306.Inverted = !SSD1306.Inverted;
-	
+
 	/* Do memory toggle */
 	for (i = 0; i < sizeof(SSD1306_Buffer); i++) {
 		SSD1306_Buffer[i] = ~SSD1306_Buffer[i];
@@ -265,12 +265,12 @@ void SSD1306_DrawPixel(uint16_t x, uint16_t y, SSD1306_COLOR_t color) {
 		/* Error */
 		return;
 	}
-	
+
 	/* Check if pixels are inverted */
 	if (SSD1306.Inverted) {
 		color = (SSD1306_COLOR_t)!color;
 	}
-	
+
 	/* Set color */
 	if (color == SSD1306_COLOR_WHITE) {
 		SSD1306_Buffer[x + (y / 8) * SSD1306_WIDTH] |= 1 << (y % 8);
@@ -287,7 +287,7 @@ void SSD1306_GotoXY(uint16_t x, uint16_t y) {
 
 char SSD1306_Putc(char ch, FontDef_t* Font, SSD1306_COLOR_t color) {
 	uint32_t i, b, j;
-	
+
 	/* Check available space in LCD */
 	if (
 		SSD1306_WIDTH <= (SSD1306.CurrentX + Font->FontWidth) ||
@@ -296,7 +296,7 @@ char SSD1306_Putc(char ch, FontDef_t* Font, SSD1306_COLOR_t color) {
 		/* Error */
 		return 0;
 	}
-	
+
 	/* Go through font */
 	for (i = 0; i < Font->FontHeight; i++) {
 		b = Font->data[(ch - 32) * Font->FontHeight + i];
@@ -308,10 +308,10 @@ char SSD1306_Putc(char ch, FontDef_t* Font, SSD1306_COLOR_t color) {
 			}
 		}
 	}
-	
+
 	/* Increase pointer */
 	SSD1306.CurrentX += Font->FontWidth;
-	
+
 	/* Return character written */
 	return ch;
 }
@@ -324,19 +324,19 @@ char SSD1306_Puts(char* str, FontDef_t* Font, SSD1306_COLOR_t color) {
 			/* Return error */
 			return *str;
 		}
-		
+
 		/* Increase string pointer */
 		str++;
 	}
-	
+
 	/* Everything OK, zero should be returned */
 	return *str;
 }
- 
+
 
 void SSD1306_DrawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, SSD1306_COLOR_t c) {
-	int16_t dx, dy, sx, sy, err, e2, i, tmp; 
-	
+	int16_t dx, dy, sx, sy, err, e2, i, tmp;
+
 	/* Check for overflow */
 	if (x0 >= SSD1306_WIDTH) {
 		x0 = SSD1306_WIDTH - 1;
@@ -350,12 +350,12 @@ void SSD1306_DrawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, SSD130
 	if (y1 >= SSD1306_HEIGHT) {
 		y1 = SSD1306_HEIGHT - 1;
 	}
-	
-	dx = (x0 < x1) ? (x1 - x0) : (x0 - x1); 
-	dy = (y0 < y1) ? (y1 - y0) : (y0 - y1); 
-	sx = (x0 < x1) ? 1 : -1; 
-	sy = (y0 < y1) ? 1 : -1; 
-	err = ((dx > dy) ? dx : -dy) / 2; 
+
+	dx = (x0 < x1) ? (x1 - x0) : (x0 - x1);
+	dy = (y0 < y1) ? (y1 - y0) : (y0 - y1);
+	sx = (x0 < x1) ? 1 : -1;
+	sy = (y0 < y1) ? 1 : -1;
+	err = ((dx > dy) ? dx : -dy) / 2;
 
 	if (dx == 0) {
 		if (y1 < y0) {
@@ -363,58 +363,58 @@ void SSD1306_DrawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, SSD130
 			y1 = y0;
 			y0 = tmp;
 		}
-		
+
 		if (x1 < x0) {
 			tmp = x1;
 			x1 = x0;
 			x0 = tmp;
 		}
-		
+
 		/* Vertical line */
 		for (i = y0; i <= y1; i++) {
 			SSD1306_DrawPixel(x0, i, c);
 		}
-		
+
 		/* Return from function */
 		return;
 	}
-	
+
 	if (dy == 0) {
 		if (y1 < y0) {
 			tmp = y1;
 			y1 = y0;
 			y0 = tmp;
 		}
-		
+
 		if (x1 < x0) {
 			tmp = x1;
 			x1 = x0;
 			x0 = tmp;
 		}
-		
+
 		/* Horizontal line */
 		for (i = x0; i <= x1; i++) {
 			SSD1306_DrawPixel(i, y0, c);
 		}
-		
+
 		/* Return from function */
 		return;
 	}
-	
+
 	while (1) {
 		SSD1306_DrawPixel(x0, y0, c);
 		if (x0 == x1 && y0 == y1) {
 			break;
 		}
-		e2 = err; 
+		e2 = err;
 		if (e2 > -dx) {
 			err -= dy;
 			x0 += sx;
-		} 
+		}
 		if (e2 < dy) {
 			err += dx;
 			y0 += sy;
-		} 
+		}
 	}
 }
 
@@ -427,7 +427,7 @@ void SSD1306_DrawRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, SSD13
 		/* Return error */
 		return;
 	}
-	
+
 	/* Check width and height */
 	if ((x + w) >= SSD1306_WIDTH) {
 		w = SSD1306_WIDTH - x;
@@ -435,7 +435,7 @@ void SSD1306_DrawRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, SSD13
 	if ((y + h) >= SSD1306_HEIGHT) {
 		h = SSD1306_HEIGHT - y;
 	}
-	
+
 	/* Draw 4 lines */
 	SSD1306_DrawLine(x, y, x + w, y, c);         /* Top line */
 	SSD1306_DrawLine(x, y + h, x + w, y + h, c); /* Bottom line */
@@ -445,7 +445,7 @@ void SSD1306_DrawRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, SSD13
 
 void SSD1306_DrawFilledRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, SSD1306_COLOR_t c) {
 	uint8_t i;
-	
+
 	/* Check input parameters */
 	if (
 		x >= SSD1306_WIDTH ||
@@ -454,7 +454,7 @@ void SSD1306_DrawFilledRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h,
 		/* Return error */
 		return;
 	}
-	
+
 	/* Check width and height */
 	if ((x + w) >= SSD1306_WIDTH) {
 		w = SSD1306_WIDTH - x;
@@ -462,7 +462,7 @@ void SSD1306_DrawFilledRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h,
 	if ((y + h) >= SSD1306_HEIGHT) {
 		h = SSD1306_HEIGHT - y;
 	}
-	
+
 	/* Draw lines */
 	for (i = 0; i <= h; i++) {
 		/* Draw lines */
@@ -479,10 +479,10 @@ void SSD1306_DrawTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, ui
 
 
 void SSD1306_DrawFilledTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3, SSD1306_COLOR_t color) {
-	int16_t deltax = 0, deltay = 0, x = 0, y = 0, xinc1 = 0, xinc2 = 0, 
-	yinc1 = 0, yinc2 = 0, den = 0, num = 0, numadd = 0, numpixels = 0, 
+	int16_t deltax = 0, deltay = 0, x = 0, y = 0, xinc1 = 0, xinc2 = 0,
+	yinc1 = 0, yinc2 = 0, den = 0, num = 0, numadd = 0, numpixels = 0,
 	curpixel = 0;
-	
+
 	deltax = ABS(x2 - x1);
 	deltay = ABS(y2 - y1);
 	x = x1;
@@ -598,7 +598,7 @@ void SSD1306_DrawFilledCircle(int16_t x0, int16_t y0, int16_t r, SSD1306_COLOR_t
         SSD1306_DrawLine(x0 + y, y0 - x, x0 - y, y0 - x, c);
     }
 }
- 
+
 
 
 void SSD1306_Clear (void)
@@ -607,22 +607,22 @@ void SSD1306_Clear (void)
     SSD1306_UpdateScreen();
 }
 void SSD1306_ON(void) {
-	SSD1306_WRITECOMMAND(0x8D);  
-	SSD1306_WRITECOMMAND(0x14);  
-	SSD1306_WRITECOMMAND(0xAF);  
+	SSD1306_WRITECOMMAND(0x8D);
+	SSD1306_WRITECOMMAND(0x14);
+	SSD1306_WRITECOMMAND(0xAF);
 }
 void SSD1306_OFF(void) {
-	SSD1306_WRITECOMMAND(0x8D);  
+	SSD1306_WRITECOMMAND(0x8D);
 	SSD1306_WRITECOMMAND(0x10);
-	SSD1306_WRITECOMMAND(0xAE);  
+	SSD1306_WRITECOMMAND(0xAE);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-//  _____ ___   _____ 
+//  _____ ___   _____
 // |_   _|__ \ / ____|
-//   | |    ) | |     
-//   | |   / /| |     
-//  _| |_ / /_| |____ 
+//   | |    ) | |
+//   | |   / /| |
+//  _| |_ / /_| |____
 // |_____|____|\_____|
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ stm32/IR_Sensor_Remote/IR_Sensor_Remote/Core/Src/main.c
@@ -0,0 +1,447 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "fonts.h"
+#include "ssd1306.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+TIM_HandleTypeDef htim1;
+
+/* USER CODE BEGIN PV */
+uint32_t tempCode;
+uint8_t bitIndex;
+uint8_t cmd;
+uint8_t cmdli;
+uint32_t code;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM1_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+	SSD1306_Init();
+	HAL_TIM_Base_Start(&htim1);
+	__HAL_TIM_SET_COUNTER(&htim1, 0);
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 71;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin : PB11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_Pin == GPIO_PIN_11) {
+		if (__HAL_TIM_GET_COUNTER(&htim1) > 8000) {
+			tempCode = 0;
+			bitIndex = 0;
+		} else if (__HAL_TIM_GET_COUNTER(&htim1) > 1700) {
+			tempCode |= (1UL << (31 - bitIndex));   // write 1
+			bitIndex++;
+		} else if (__HAL_TIM_GET_COUNTER(&htim1) > 1000) {
+			tempCode &= ~(1UL << (31 - bitIndex));  // write 0
+			bitIndex++;
+		}
+		if (bitIndex == 32) {
+			cmdli = ~tempCode; // Logical inverted last 8 bits
+			cmd = tempCode >> 8; // Second last 8 bits
+			if (cmdli == cmd) // Check for errors
+					{
+				code = tempCode; // If no bit errors
+				// Do your main work HERE
+//no display
+//        SSD1306_Clear();
+//        SSD1306_GotoXY (0, 20);
+				char snum[10];
+				itoa(code, snum, 10);
+//no display
+//        SSD1306_Puts (snum, &Font_11x18, 1);
+//        SSD1306_UpdateScreen();
+			}
+			bitIndex = 0;
+		}
+		__HAL_TIM_SET_COUNTER(&htim1, 0);
+	}
+}
+/*
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_Pin == GPIO_PIN_11) {
+		if (__HAL_TIM_GET_COUNTER(&htim1) > 8000) {
+			tempCode = 0;
+			bitIndex = 0;
+		} else if (__HAL_TIM_GET_COUNTER(&htim1) > 1700) {
+			tempCode |= (1UL << (31 - bitIndex));   // write 1
+			bitIndex++;
+		} else if (__HAL_TIM_GET_COUNTER(&htim1) > 1000) {
+			tempCode &= ~(1UL << (31 - bitIndex));  // write 0
+			bitIndex++;
+		}
+		if (bitIndex == 32) {
+			cmdli = ~tempCode; // Logical inverted last 8 bits
+			cmd = tempCode >> 8; // Second last 8 bits
+			if (cmdli == cmd) // Check for errors
+					{
+				code = tempCode; // If no bit errors
+				// Do your main work HERE
+				SSD1306_Clear();
+				SSD1306_GotoXY(0, 20);
+				switch (code) {
+				case (16753245):
+					SSD1306_Puts("CH-", &Font_16x26, 1);
+					break;
+
+				case (16736925):
+					SSD1306_Puts("CH", &Font_16x26, 1);
+					break;
+
+				case (16769565):
+					SSD1306_Puts("CH+", &Font_16x26, 1);
+					break;
+
+				case (16720605):
+					SSD1306_Puts("|<<", &Font_16x26, 1);
+					break;
+
+				case (16712445):
+					SSD1306_Puts(">>|", &Font_16x26, 1);
+					break;
+
+				case (16761405):
+					SSD1306_Puts(">||", &Font_16x26, 1);
+					break;
+
+				case (16769055):
+					SSD1306_Puts("-", &Font_16x26, 1);
+					break;
+
+				case (16754775):
+					SSD1306_Puts("+", &Font_16x26, 1);
+					break;
+
+				case (16748655):
+					SSD1306_Puts("EQ", &Font_16x26, 1);
+					break;
+
+				case (16738455):
+					SSD1306_Puts("0", &Font_16x26, 1);
+					break;
+
+				case (16750695):
+					SSD1306_Puts("100+", &Font_16x26, 1);
+					break;
+
+				case (16756815):
+					SSD1306_Puts("200+", &Font_16x26, 1);
+					break;
+
+				case (16724175):
+					SSD1306_Puts("1", &Font_16x26, 1);
+					break;
+
+				case (16718055):
+					SSD1306_Puts("2", &Font_16x26, 1);
+					break;
+
+				case (16743045):
+					SSD1306_Puts("3", &Font_16x26, 1);
+					break;
+
+				case (16716015):
+					SSD1306_Puts("4", &Font_16x26, 1);
+					break;
+
+				case (16726215):
+					SSD1306_Puts("5", &Font_16x26, 1);
+					break;
+
+				case (16734885):
+					SSD1306_Puts("6", &Font_16x26, 1);
+					break;
+
+				case (16728765):
+					SSD1306_Puts("7", &Font_16x26, 1);
+					break;
+
+				case (16730805):
+					SSD1306_Puts("8", &Font_16x26, 1);
+					break;
+
+				case (16732845):
+					SSD1306_Puts("9", &Font_16x26, 1);
+					break;
+
+				default:
+					break;
+				}
+				SSD1306_UpdateScreen();
+			}
+			bitIndex = 0;
+		}
+		__HAL_TIM_SET_COUNTER(&htim1, 0);
+	}
+}
+*/
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
